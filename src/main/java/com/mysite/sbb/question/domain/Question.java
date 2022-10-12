4e46fa513fd97c946bf266b824373464e7429647
xanqus <sbk98031816@gmail.com>
2022-10-12 15:43:46 +0900

    질문 수정

--- src/main/java/com/mysite/sbb/question/domain/Question.java
+++ src/main/java/com/mysite/sbb/question/domain/Question.java
@@ -26,6 +26,8 @@ public class Question {
 
     private LocalDateTime createDate;
 
+    private LocalDateTime modifyDate;
+
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     @JsonIgnore
     private List<Answer> answerList;
